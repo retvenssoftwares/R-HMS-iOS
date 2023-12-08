48d9cb462a8918c1e7cd3649d4493af206d265e0
mayurkumar2068 <mayurkumar2068@gmail.com>
2023-12-08 19:42:29 +0530

    Onboarding Fixed

---
+++ Hotel Management System/Controllers/Create Property/Cell/Test/Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by mayur bobade on 06/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
